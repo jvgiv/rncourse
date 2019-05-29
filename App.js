d194617ca6ddf5788d8d39f989e77c008fec3e72
jvgiv <jvgiv12@gmail.com>
2019-05-28 22:58:43 -0400

    Checking in w a list making phone app

---
+++ App.js
@@ -0,0 +1,93 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  state = {
+    placeName: '',
+    places: []
+  }
+
+  placeNameChangedHandler = (e) => {
+    this.setState({
+      placeName: e
+    })
+  }
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    } 
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName),
+        placeName: ''
+      }
+    })
+  }
+
+  render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ))
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangedHandler}
+            />
+          <Button 
+            style={styles.placeButton} 
+            title="Add" 
+            onPress={this.placeSubmitHandler} 
+          />
+        </View>
+        <View>
+          {placesOutput}
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 26,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  inputContainer: {
+    // flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  placeInput: {
+    width: '70%'
+  },
+  placeButton: {
+    width: "30%"
+  }
+});
